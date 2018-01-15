7fc941af4b1bfc7e591bbf424a2b613aa4966994
RAHUL SHRIRAO <W10@192.168.1.206>
2018-01-15 15:16:48 +0530

    Adding Item-- validation,update and delete operation with table view

--- src/com/Controller/Ui/ItemsUi.java
+++ src/com/Controller/Ui/ItemsUi.java
@@ -396,8 +396,8 @@ public class ItemsUi extends javax.swing.JInternalFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 427, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 511, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 27, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 51, Short.MAX_VALUE))
         );
 
         pack();
